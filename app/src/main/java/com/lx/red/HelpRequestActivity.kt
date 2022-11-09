006eab4d251042343c622067d4a49e147d193f1b
Hyunseung Jang <ekrcu9988@gmail.com>
2022-11-09 22:35:25 +0900

    현승 - 긴급요청 메시지

--- app/src/main/java/com/lx/red/HelpRequestActivity.kt
+++ app/src/main/java/com/lx/red/HelpRequestActivity.kt
@@ -1,21 +1,46 @@
 package com.lx.red
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.lx.red.databinding.ActivityHelpRequestBinding
+import kotlinx.android.synthetic.main.activity_help_request.*
 
 class HelpRequestActivity : AppCompatActivity() {
     lateinit var binding : ActivityHelpRequestBinding
+    var helpCheck = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHelpRequestBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        object : CountDownTimer(10000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                count.text = "" + millisUntilFinished / 1000
+            }
+
+            override fun onFinish() {
+                if(helpCheck==2) {
+
+                } else {
+                    run()
+                }
+            }
+        }.start()
+
         binding.confirmhelpButton.setOnClickListener {
-            val intent = Intent(this,ConfirmationHelpActivity::class.java)
+            val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
+            helpCheck = 2
         }
+    }
+
+    fun run() {
+        val intent = Intent(this,ConfirmationHelpActivity::class.java)
+        startActivity(intent)
     }
 }
