f7f0f653b2df5486173bca112d2d7cd548379bf8
seungho <ekrcu9988@gmail.com>
2022-11-10 14:51:21 +0900

    상단바 삭제

--- app/src/main/java/com/lx/red/MyInfoMainActivity.kt
+++ app/src/main/java/com/lx/red/MyInfoMainActivity.kt
@@ -4,19 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lx.red.databinding.ActivityMyInfoMainBinding
+import com.lx.red.databinding.ActivityMyInfoUpdateBinding
 
 class MyInfoMainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMyInfoMainBinding
+    lateinit var binding: ActivityMyInfoMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMyInfoMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //긴급전화 등록
-        binding.addcallButton.setOnClickListener {
-            val intent = Intent(this,EmergencyCallActivity::class.java)
+        binding.outputName.text = "${MemberData.memberId+"의 정보"}"
+        binding.outputTall.text= "${MemberData.memberHeight}"
+        binding.outputWeight.text= "${MemberData.memberWeight}"
+        binding.outputMedi.text= "${MemberData.memberMedicine}"
+        binding.outputBlood.text= "${MemberData.memberBloodtype}"
+        binding.outputSick.text= "${MemberData.memberDisease}"
+        binding.outputOther.text= "${MemberData.memberOther}"
+        binding.outputCertifi.text= "${MemberData.memberCertificate}"
+        binding.outputEmer.text= "${MemberData.memberEmernum}"
+        //내정보
+        binding.goupdateButton.setOnClickListener {
+            val intent = Intent(this,MyInfoUpdateActivity::class.java)
             startActivity(intent)
         }
+
     }
+
 }
+
+
+
---
+++ app/src/main/java/com/lx/red/MyInfoUpdateActivity.kt
@@ -0,0 +1,91 @@
+package com.lx.red
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.lx.api.BasicClient
+import com.lx.data.MemberListResponse
+import com.lx.red.databinding.ActivityMyInfoUpdateBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MyInfoUpdateActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMyInfoUpdateBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMyInfoUpdateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.outputName.text = "${MemberData.memberId+"의 정보"}"
+//        binding.infoinputTall.text= "${MemberData.memberHeight}"
+//        binding.infoinputWeight.text= "${MemberData.memberWeight}"
+//        binding.infoinputMedi.text= "${MemberData.memberMedicine}"
+//        binding.infoinputBlood.text= "${MemberData.memberBloodtype}"
+//        binding.infoinputSick.text= "${MemberData.memberDisease}"
+//        binding.infoinputOther.text= "${MemberData.memberOther}"
+//        binding.infoinputCertifi.text= "${MemberData.memberCertificate}"
+//        binding.infoinputEmer.text= "${MemberData.memberEmernum}"
+
+
+
+        //긴급전화 등록
+        binding.addcallButton.setOnClickListener {
+            val intent = Intent(this, EmergencyCallActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.updateButton.setOnClickListener {
+            updateMember()
+
+        }
+        binding.updateButton.setOnClickListener {
+            val intent = Intent(this,MyInfoMainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    //회원 정보 수정 요청하기
+    fun updateMember() {
+        val memberWeight = binding.infoinputWeight.text.toString()
+        val memberHeight = binding.infoinputTall.text.toString()
+        val memberCertificate = binding.infoinputCertifi.text.toString()
+        val memberMedicine = binding.infoinputMedi.text.toString()
+        val memberDisease = binding.infoinputSick.text.toString()
+        val memberEmernum = binding.infoinputEmer.text.toString()
+        val memberOther = binding.infoinputOther.text.toString()
+        val memberId = binding.outputName.text.toString()
+        val memberBlood = binding.infoinputBlood.text.toString()
+
+        BasicClient.api.memberUpdate(
+            requestCode = "1001",
+            id = memberId,
+            height = memberHeight,
+            weight = memberWeight,
+            certificate = memberCertificate,
+            medicine = memberMedicine,
+            disease = memberDisease,
+            emernum = memberEmernum,
+            others = memberOther,
+            bloodtype = memberBlood
+        ).enqueue(object : Callback<MemberListResponse> {
+            override fun onResponse(call: Call<MemberListResponse>, response: Response<MemberListResponse>) {
+
+            }
+            override fun onFailure(call: Call<MemberListResponse>, t: Throwable) {
+
+            }
+
+        })
+
+    }
+
+    fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+}
